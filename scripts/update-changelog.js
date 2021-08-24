1a6fae98183f6a88b5f375b0828e9118eb351079
Vladimir Razuvaev <vladimir.razuvaev@gmail.com>
2021-08-25 02:59:03 +0700

    update

--- scripts/update-changelog.js
+++ scripts/update-changelog.js
@@ -1,6 +1,9 @@
 const path = require(`path`)
 const fs = require(`fs`)
+const childProcess = require(`child_process`)
 
 const changelogPath = path.join(process.cwd(), `packages`, `my-test-package2`, `CHANGELOG.md`)
 const content = fs.readFileSync(changelogPath)
 fs.writeFileSync(changelogPath, `lalala` + String(content))
+
+childProcess.execSync("git", ["add", "--", changelogPath]);
