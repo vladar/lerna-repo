713a8da2719e21c8443cdb1e7f94b6b71889a2eb
Vladimir Razuvaev <vladimir.razuvaev@gmail.com>
2021-08-25 14:35:52 +0700

    update

--- scripts/update-changelog.js
+++ scripts/update-changelog.js
@@ -3,13 +3,36 @@ const fs = require(`fs`)
 const childProcess = require(`child_process`)
 const execa = require(`execa`)
 
+function toPackageName(path) {
+  const parts = path.split(/[\\/]/)
+  return parts[parts.length - 1] === `package.json` && parts[parts.length - 3] === `packages`
+    ? parts[parts.length - 2]
+    : undefined
+}
+
 async function run() {
   const { stdout } = await execa("git", ["ls-files", "-m"])
-  console.log(`Changed files: `, String(stdout))
+  // 88cc97089cbabb0edce0383fe6132ef79aaeaa69
 
-  const changelogPath = path.join(process.cwd(), `packages`, `my-test-package2`, `CHANGELOG.md`)
-  const content = fs.readFileSync(changelogPath)
-  fs.writeFileSync(changelogPath, `lalala` + String(content))
+  const changedPackages = String(stdout)
+    .split(`\n`)
+    .map(toPackageName)
+    .filter(Boolean)
+
+  if (!changedPackages.length) {
+
+  }
+
+  console.log(`Changed packages: `, changedPackages)
+
+  for (const packageName of changedPackages) {
+    const changelogPath = path.join(process.cwd(), `packages`, packageName, `CHANGELOG.md`)
+    const content = fs.readFileSync(changelogPath)
+    fs.writeFileSync(changelogPath, `lalala` + String(content))
+  }
+
+
+
 
   await execa("git", ["add", changelogPath]);
 }
