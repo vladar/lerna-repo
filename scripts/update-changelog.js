b01ac14f59bb7a1260108a544c538be76a21f0ef
Vladimir Razuvaev <vladimir.razuvaev@gmail.com>
2021-08-25 14:27:59 +0700

    update

--- scripts/update-changelog.js
+++ scripts/update-changelog.js
@@ -4,6 +4,9 @@ const childProcess = require(`child_process`)
 const execa = require(`execa`)
 
 async function run() {
+  const { stdout } = await execa("git", ["ls-files", "-m"])
+  console.log(`Changed files: `, String(stdout))
+
   const changelogPath = path.join(process.cwd(), `packages`, `my-test-package2`, `CHANGELOG.md`)
   const content = fs.readFileSync(changelogPath)
   fs.writeFileSync(changelogPath, `lalala` + String(content))
